23cae2b5d67cbf9cfcbd38c80b5d636268e8ff9f
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-04-26 23:35:02 +0530

    css part global and nodemodules

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+
 export default function Home() {
   return (
    <>
+   <div className="navigation"><div className="header">
+   
    <h1>Home page</h1>
-   <ul>
-    <li><Link href="/">Home</Link></li>
-    <li><Link href="/services">Service</Link></li>
-    <li><Link href="/products">Products</Link></li>
-    <li><Link href="/About">About</Link></li>
-    <li><Link href="/contact">Contact Us</Link></li>
-    <li><Link href="/About/branches">Branches</Link></li>
+   </div>
+   <div>
+   <ul className="body">
+    <li><Link href="/" className={styles.link}>Home</Link></li>
+    <li><Link href="/services" className={styles.link}>Service</Link></li>
+    <li><Link href="/products" className={styles.link}>Products</Link></li>
+    <li><Link href="/About" className={styles.link}>About</Link></li>
+    <li><Link href="/contact" className={styles.link}>Contact Us</Link></li>
+    <li><Link href="/About/branches" className={styles.link}>Branches</Link></li>
     
    </ul>
-   <Link href="https://nextjs.org" target="_blanks">NextJS</Link>
+   </div>
+   </div>
+   <Link href="https://nextjs.org" target="_blanks" className={styles.link}>NextJS</Link>
+   
    </>
   )
 }
